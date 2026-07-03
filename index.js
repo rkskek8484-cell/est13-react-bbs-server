e42210cdda5f1fd3907740221e95b0e55672f0c4
rkskek8484-cell <rkskek8484@gmail.com>
2026-07-03 15:10:48 +0900

    feat: add POST /update endpoint to modify existing board posts

--- index.js
+++ index.js
@@ -54,6 +54,16 @@ app.post('/write', (req, res) => {
     res.send(result);
   });
 });
+app.post('/update', (req, res) => {
+  console.log(req.body);
+  const { name, title, content, id } = req.body;
+
+  const sqlQuery = 'UPDATE board SET writer=?, title=?, content=? WHERE id=?';
+  db.query(sqlQuery, [name, title, content, id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
